c4bb672f7282742498443795f13ed5ccab5482d0
taha habibi <mohammadreza.habibi7777777@gmail.com>
2024-05-26 17:22:41 +0330

    initial commit

---
+++ app/src/main/java/com/king/android/p06_first_git_projeect/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.king.android.p06_first_git_projeect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
